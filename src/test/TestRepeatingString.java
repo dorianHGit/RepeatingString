00a67bbc37967b9044bc4e0e4736da228fa0efec
dorianHGit <thepictueofdoriansinbox@outlook.com>
2020-02-06 15:24:47 +0000

    Outlined Test Cases

---
+++ src/test/TestRepeatingString.java
@@ -0,0 +1,46 @@
+package test;
+
+import org.junit.Test;
+
+public class TestRepeatingString {
+
+    @Test
+    public void testEmptyStringThrowsException(){
+
+    }
+
+    @Test
+    public void testStringLongerThanOneHundredThrowsException(){
+
+    }
+
+    @Test
+    public void testRepeatingStringLengthLessThanOneThrowsException(){
+
+    }
+
+    @Test
+    public void testRepeatingStringLengthGreaterThanTenPowTwelveThrowsException(){
+
+    }
+
+    @Test
+    public void testMinimumStringSizeRuns(){
+
+    }
+
+    @Test
+    public void testMaximumStringSizeRuns(){
+
+    }
+
+    @Test
+    public void testMinimumRepeatSizeRuns(){
+
+    }
+
+    @Test
+    public void testMaximumRepeatSizeRuns(){
+
+    }
+}
